424fa6b5cbe4594decab21d7433c62405fc8d44d
Bolade Olufayo Taiwo <olufayobolade@gmail.com>
2025-09-22 13:31:48 +0100

    added images

---
+++ public/tailwind.config.ts
@@ -0,0 +1,13 @@
+// module.exports = {
+//     darkMode: "class", // add this
+//     content: [
+//       "./pages/**/*.{js,ts,jsx,tsx}",
+//       "./components/**/*.{js,ts,jsx,tsx}",
+//       "./app/**/*.{js,ts,jsx,tsx}",
+//     ],
+//     theme: {
+//       extend: {},
+//     },
+//     plugins: [],
+//   };
+  
